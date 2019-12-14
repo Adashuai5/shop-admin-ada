514b01787534df185945fac4d23ef78c6785a28a
adashuai5 <ad873606339@gmail.com>
2019-12-14 21:24:05 +0800

    add menu

--- src/main.js
+++ src/main.js
@@ -1,19 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './assets/css/global.less'
+import './assets/fonts/iconfont.css'
+import Element from 'element-ui';
 
 import axios from 'axios'
 
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  // 为请求头添加 Token 验证的 Authorization 字段
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+
 Vue.prototype.$http = axios
 
-import Element from 'element-ui';
 Vue.use(Element, {
   zIndex: 3000
-});
-
-import './assets/css/global.less'
-import './assets/fonts/iconfont.css'
+})
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,153 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container>
+    <el-aside :width="isCollapse?'64px':'256px'">
+      <div class="logo-wrapper">
+        <router-link to="/home">
+          <img src="../assets/logo.png" alt />
+          <h1 v-show="!isCollapse">Ada 后台管理系统</h1>
+        </router-link>
+      </div>
+      <el-menu
+        @open="handleOpen"
+        @close="handleClose"
+        background-color="#001529"
+        text-color="#909399"
+        active-text-color="#fff"
+        unique-opened
+        :collapse="isCollapse"
+        :collapse-transition="false"
+      >
+        <el-submenu :index="item.id+''" v-for="item in menuList" :key="item.id">
+          <template slot="title">
+            <i class="iconfont mr-10" :class="'icon'+ item.path"></i>
+            <span class="title-text">{{item.authName}}</span>
+          </template>
+          <el-menu-item :index="subItem.id+''" v-for="subItem in item.children" :key="subItem.id">
+            <template slot="title">
+              <i class="el-icon-menu"></i>
+              <span>{{subItem.authName}}</span>
+            </template>
+          </el-menu-item>
+        </el-submenu>
+      </el-menu>
+    </el-aside>
+    <el-container>
+      <el-header>
+        <div class="swich el-icon-s-fold" @click="doSwitch" v-if="isCollapse === false"></div>
+        <div class="swich el-icon-s-unfold" @click="doSwitch" v-else></div>
+        <div>
+          <el-button size="mini" type="info" @click="logout">退出</el-button>
+        </div>
+      </el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 <script>
 export default {
+  data() {
+    return {
+      menuList: [],
+      isCollapse: true
+    }
+  },
+  created() {
+    this.getMenuList()
+  },
   methods: {
     logout() {
       window.sessionStorage.clear('token')
       this.$router.push('/login')
+    },
+    async getMenuList() {
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.menuList = res.data
+    },
+    doSwitch() {
+      this.isCollapse = !this.isCollapse
+    },
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath)
     }
   }
 }
 </script>
+<style lang="less" scoped>
+.el-header {
+  height: 64px;
+  background: #fff;
+  box-shadow: 0 1px 4px rgba(0, 21, 41, 0.08);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  .swich {
+    margin-left: -20px;
+    font-size: 20px;
+    cursor: pointer;
+    padding: 22px 24px;
+    &:hover {
+      background-color: #f0f2f5;
+    }
+  }
+}
+
+.el-aside {
+  min-height: 100vh;
+  box-shadow: 2px 0 6px rgba(0, 21, 41, 0.35);
+  background: #001529;
+  .el-menu {
+    border-right: none;
+    .el-submenu {
+      &:hover,
+      &.is-active,
+      &.is-open {
+        .iconfont,
+        .title-text {
+          color: #fff !important;
+        }
+      }
+    }
+    .el-menu-item {
+      &:hover {
+        i,
+        span {
+          color: #fff !important;
+        }
+      }
+      &.is-active {
+        background: #1890ff !important;
+      }
+    }
+  }
+
+  .logo-wrapper {
+    background: #002140;
+    width: 100%;
+    height: 64px;
+    padding-left: 16px;
+    line-height: 64px;
+    img {
+      display: inline-block;
+      vertical-align: middle;
+      height: 32px;
+    }
+    h1 {
+      color: #fff;
+      display: inline-block;
+      vertical-align: middle;
+      font-size: 20px;
+      margin: 0 0 0 16px;
+      font-weight: 600;
+    }
+  }
+}
+
+.el-main {
+  background: #f0f2f5;
+  color: #333;
+}
+</style>
